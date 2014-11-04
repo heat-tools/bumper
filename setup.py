ece4c8fae6a660175cafaa54b91ba3ca1a17c6af
Jason Boyles <jason@duncancreek.net>
2014-11-04 10:50:34 -0600

    make pip-installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='bumper',
+      description='manipulate tags on Github hosted repos',
+      version='0.1.0',
+      author='Rackspace',
+      author_email='jason@duncancreek.net',
+      license='Apache License (2.0)',
+      classifiers=["Programming Language :: Python"],
+      url='https://github.com/JasonBoyles/bumper',
+      scripts=['bumper'],
+      install_requires=['click==3.3', 'PyGithub==1.25.2']
+      )
