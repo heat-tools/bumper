a2e3eb9fb17f68eb01c81fdfc240818933ad933f
Jason Boyles <jason@duncancreek.net>
2015-01-23 17:29:07 -0600

    bump version, update install_requires

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from distutils.core import setup
 
 setup(name='bumper',
       description='manipulate tags on Github hosted repos',
-      version='0.1.2',
+      version='0.1.3',
       author='Rackspace',
       author_email='jason@duncancreek.net',
       license='Apache License (2.0)',
       classifiers=["Programming Language :: Python"],
       url='https://github.com/JasonBoyles/bumper',
       scripts=['bumper'],
-      install_requires=['click==3.3', 'PyGithub==1.25.2']
+      install_requires=['click==3.3',
+                        'PyGithub==1.25.2',
+                        'hot>=0.5.0',
+                        'requests>=2.4.0']
       )
